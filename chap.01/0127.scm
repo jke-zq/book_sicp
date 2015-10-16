c2af69d62468f2ffa68e9267319ea1f1975cb1e0
jke0zq <jke0zq@gmail.com>
2015-10-16 18:04:36 +0800

    good codes

---
+++ chap.01/0127.scm
@@ -0,0 +1,23 @@
+#lang racket
+;;good code style
+(define (square n)
+  (* n n))
+
+(define (expmod base exp m)
+  (cond 
+    ((= exp 0) 1)
+    ((even? exp)
+      (remainder (square (expmod base (/ exp 2) m)) 
+                 m))
+    (else
+      (remainder (* base (expmod base (- exp 1) m))
+                 m))))
+(define (fermat-test a n)
+  (= (expmod a n n) a))
+
+(define (fast-prime? n)
+  (define (fast-prime-iter acc)
+    (cond ((= acc n) true)
+          ((fermat-test acc n) (fast-prime-iter (+ acc 1)))
+          (else false)))
+  (fast-prime-iter 1))
