d120a5ace87389cc0cd01ea5fa4ff0b85405f314
jke0zq <jke0zq@gmail.com>
2016-01-24 22:33:27 +0800

    to 0286

--- chap.02/0281.rkt
+++ chap.02/0281.rkt
@@ -247,3 +247,39 @@
         (if proc
             (apply proc (map contents args))
             (apply-with-raised-types args)))))
+;;0286
+;;change the + - * / to add sub mul div to deal with other types, such as schemer-number\rational etc.
+
+(define (add-complex z1 z2)
+  (make-from-real-imag (add (real-part z1) (real-part z2))
+                       (add (imag-part z1) (imag-part z2)))
+
+(define (sub-complex z1 z2)
+  (make-from-real-imag (sub (real-part z1) (real-part z2))
+                       (sub (imag-part z1) (imag-part z2)))
+
+(define (mul-complex z1 z2)
+  (make-from-mag-ang (mul (magnitude z1) (magnitude z2))
+                     (add (angle z1) (angle z2)))
+
+(define (div-complex z1 z2)
+  (make-from-mag-ang (div (magnitude z1) (magnitude z2))
+                     (sub (angle z1) (angle z2)))
+;;;add into global
+(define (sine x) (apply-generic 'sine x)) 
+(define (cosine x) (apply-generic 'cosine x))
+;;;; add into scheme-number package 
+(put 'sine 'scheme-number 
+       (lambda (x) (tag (sin x)))) 
+(put 'cosine 'scheme-number 
+     (lambda (x) (tag (cos x)))) 
+
+;;;; add into rational package 
+(put 'sine 'rational 
+     (lambda (x) (tag (sin x)))) 
+(put 'cosine 'rational 
+     (lambda (x) (tag (cos x)))) 
+;;;other operators needed in the other operators of complex type will deal with schemer-number and rational.
+  
+
+  
