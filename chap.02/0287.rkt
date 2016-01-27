08aef5931f163581849c9c6ed9f55192a8090d45
jke0zq <jke0zq@gmail.com>
2016-01-27 22:00:55 +0800

    to 0291

--- chap.02/0287.rkt
+++ chap.02/0287.rkt
@@ -32,3 +32,32 @@
                 ((< exponent exp) (cons (car term-list) (iter (- exp 1) (cdr term-list))))
                 (else (iter (+ exp 1) (cons 0 term-list)))))
         (iter (- len 1) term-list))))
+;;0290
+;;;solution one: find another package
+;;;soluition two: distinguish the first-term and adjoin-term, but also install another package
+
+;;0291
+(define (div-terms l1 l2)  
+   (if (empty-termlist? l1) 
+     (list (the-empty-termlist) (the-empty-termlist)) 
+     (let ((t1 (first-term l1)) 
+           (t2 (first-term l2))) 
+       (if (> (order t2) (order t1)) 
+         (list (the-empty-termlist) l1) 
+         (let ((first-term (make-term (- (order t1) (order t2)) 
+                                      (div (coeff t1) (coeff t2))))) 
+           (let ((rest-of-result 
+                   (div-terms (sub-terms l1 
+                                         (mul-term-by-all-terms first-term 
+                                                                l2)) 
+                              l2))) 
+             (list (adjoin-term first-term (car rest-of-result)) 
+                   (cadr rest-of-result)))))))) 
+  
+(define (div-poly p1 p2) 
+  (if (same-variable? (variable p1) (variable p2)) 
+      (let ((results (div-terms (term-list p1) 
+                                (term-list p2)))) 
+        (list (make-poly (variable p1) (car results)) (cadr results))) 
+      (error "Polys not in same var -- div-poly" 
+             (list p1 p2))))
